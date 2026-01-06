d1af9358d95ecc918c3d63d39ccff48d9f319f50
HaoNan Tang <lancera.thn@gmail.com>
2026-01-06 21:07:24 +0800

    add wasb, ready 2 train

--- models_factory/backbones/__init__.py
+++ models_factory/backbones/__init__.py
@@ -2,8 +2,10 @@
 
 from .tracknetv2_backbone import TrackNetV2Backbone
 from .tracknetv4_backbone import TrackNetV4Backbone
+from .wasb_hrnet_backbone import WASBHRNetBackbone
 
 __all__ = [
     'TrackNetV2Backbone',
-    'TrackNetV4Backbone'
+    'TrackNetV4Backbone',
+    'WASBHRNetBackbone'
 ]
